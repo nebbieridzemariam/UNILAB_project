5f6ce68af0a7b22fff950d59f7d837e2edc07649
nebbieridzemariam <94562268+nebbieridzemariam@users.noreply.github.com>
2022-06-17 23:53:09 +0400

    added burger menu footer

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,16 @@
 function openBurgerMenu() {
   document.getElementById("burgerNavigation").style.transform = "none";
+  document.body.style.overflow = "hidden";
 }
 
 function closeBurgerMenu() {
   document.getElementById("burgerNavigation").style.transform =
     "translate(-100%, 0)";
+  document.body.style.overflow = "unset";
 }
 
 function loadBody() {
   for (let section of document.getElementsByTagName("section")) {
     section.onclick = closeBurgerMenu;
   }
-
-  document.getElementById("burgerNavigation").style.height =
-    document.body.clientHeight + "px";
 }
